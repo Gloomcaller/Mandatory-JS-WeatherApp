1d570b5942a963a9b0dfa93c02c647859b6ed55a
Vojislav Beli <94176223+Gloomcaller@users.noreply.github.com>
2025-01-24 15:11:48 +0100

    got the fetch to work with my API

--- index.js
+++ index.js
@@ -3,22 +3,33 @@ const cityInput = document.querySelector(".cityInput");
 const card = document.querySelector(".card");
 const apiKey = apiKey;
 
-waetherForm.addEventListener("submit", event => {
+waetherForm.addEventListener("submit", async event => {
     event.preventDefault();
     const city = cityInput.value;
     if (city) {
-
+        try {
+            const weatherData = await getWeatherData(city);
+            displayWeatherInfo(weatherData);
+        } catch (error) {
+            console.error(error);
+            displayError(error);
+        }
     } else {
         displayError("Please enter a city");
     }
 });
 
 async function getWeatherData(city) {
-
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    const response = await fetch(apiUrl);
+    if (!response.ok) {
+        throw new Error("Could not fetch weather data");
+    }
+    return await response.json();
 }
 
 function displayWeatherInfo(data) {
-
+    
 }
 
 function getWeatherEmoji(weatherID) {
